a31032b31c9f266c0b28d7240bc0f6c2cb239431
arthur andrade nunes <nunesarthur252@gmail.com>
2024-12-04 00:28:20 +0000

    finish endpoint /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,15 +1,20 @@
 import database from "infra/database.js";
-import { version } from "react";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  try {
+
     const databaseVersion = await database.query("SHOW server_version");
     const databaseVersionValue = databaseVersion.rows[0].server_version;
 
     const databaseMaxConnections = await database.query("SHOW max_connections");
-    const databaseMaxConnectionsValue =
-      databaseMaxConnections.rows[0].max_connections;
+    const databaseMaxConnectionsValue = databaseMaxConnections.rows[0].max_connections;
+    
+    const databaseName = process.env.POSTGRES_DB
+    const databaseOpenedConnectionsResult = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName]
+    });
+    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
 
     response.status(200).json({
       updated_at: updatedAt,
@@ -17,14 +22,10 @@ export default async function status(request, response) {
         database: {
           version: databaseVersionValue,
           max_connections: parseInt(databaseMaxConnectionsValue),
+          opened_connections: databaseOpenedConnectionsValue
         },
       },
     });
-  } catch (err) {
-    console.error(err);
-  } finally {
-    database.end();
-  }
 }
 /**
   * curl http://localhost:3000/api/status -v
--- test/integrations/api/v1/status/get.test.js
+++ test/integrations/api/v1/status/get.test.js
@@ -8,6 +8,8 @@ test("GET to /api/v1/status should return 200", async () => {
 
   expect(responseBody.dependencies.database.version).toBe("16.0");
   expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
   console.log(responseBody);
 });
-msm;
+
+
