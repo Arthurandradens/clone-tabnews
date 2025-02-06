f732531e0d49f43b015625ebfcaaa69f942fb353
arthurandradens <nunesarthur252@gmail.com>
2025-02-06 17:50:57 -0300

    adds wait-for-postgres.js script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,15 @@
+const {exec } = require("node:child_process");
+function checkPostgres(){
+    exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+    function handleReturn(error,stdout){
+        if(stdout.search("accepting connections") === -1){
+            process.stdout.write(".");
+            checkPostgres();
+            return;
+        }
+        console.log("\n Postgres is ready");    
+    }
+}
+process.stdout.write("\n\n aguardando postgres aceitar conexões");
+checkPostgres();
