b79446e5ff6e76d5a43a9b9d7c403429ecf363b6
arthurmultidrop <arthurnunes@multidrop.com>
2025-11-27 14:40:55 -0300

    console.log no servidor

--- test/integrations/api/v1/migrations/get.test.js
+++ test/integrations/api/v1/migrations/get.test.js
@@ -8,6 +8,7 @@ beforeAll(async () => {
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
+  console.log(response);
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
