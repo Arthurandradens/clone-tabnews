73c5ff207d7023b248832aa827581aa839fdbd04
arthurmultidrop <arthurnunes@multidrop.com>
2025-11-27 11:47:09 -0300

    mudança no database.js

--- infra/database.js
+++ infra/database.js
@@ -1,14 +1,14 @@
 import { Client } from "pg";
+
 async function query(queryObject) {
   let client;
-
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
-  } catch (err) {
-    console.error(err);
-    throw err;
+  } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
@@ -28,12 +28,17 @@ async function getNewClient() {
   return client;
 }
 
-function getSSLValues() {
-  return process.env.NODE_ENV === "production";
-}
-const database = {
+export default {
   query,
   getNewClient,
 };
 
-export default database;
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
